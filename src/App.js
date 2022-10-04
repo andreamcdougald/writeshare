df556f093acd502dabbd29d0fab3f3c05306d82e
tjmac <teajay@livingskytech.com>
2022-10-04 01:37:38 -0400

    add price at bottom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "./App.css";
+import React, { useState, useEffect, useContext } from "react";
 import Hero from "./components/Hero";
 import Intro from "./components/Intro";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Testimonial from "./components/Testimonial";
+import PriceContext from "./components/PriceGenerator";
 
 function App() {
+  const [price] = useContext(PriceContext);
+  const [openPrice, setOpenPrice] = useState(false);
+  const [viewedPrice, setViewedPrice] = useState(false);
+
+  useEffect(() => {
+    if (openPrice) {
+      if (window.gtag) {
+        window.gtag("event", "price_click", {
+          price,
+        });
+      }
+      setViewedPrice(true);
+    }
+  }, [openPrice, price]);
   return (
     <div className="App">
-      <Hero />
+      <Hero openPrice={openPrice} setOpenPrice={setOpenPrice} viewedPrice={viewedPrice} />
       <Intro />
       <Features />
       <Testimonial />
-      <Footer />
+      <Footer openPrice={openPrice} setOpenPrice={setOpenPrice} viewedPrice={viewedPrice} />
     </div>
   );
 }
--- src/components/ShowPrice.js
+++ src/components/ShowPrice.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PriceContext from "./PriceGenerator";
 import { useContext } from "react";
 
-export default function ShowPrice() {
+export default function ShowPrice(props) {
   const [price] = useContext(PriceContext);
-  const [open, setOpen] = useState(false);
-
-  function addToDataLayer() {
-    if (window.gtag) {
-      window.gtag("event", "price_click", {
-        price,
-      });
-    }
-  }
-  localStorage.setItem("viewedPrice", "false");
-  /* eslint-disable */
-  useEffect(() => {
-    if (open) {
-      addToDataLayer();
-      localStorage.setItem("viewedPrice", "true");
-    }
-  }, [open]);
 
   return (
-    <div id="price" className="" onClick={() => setOpen(true)}>
-      {open ? (
+    <div id="price" className="" onClick={() => props.setOpenPrice(true)}>
+      {props.openPrice ? (
         <div className="">
           <div className="animate-[moveout_.2s_ease-out_forwards]">
             <h2 className="min-w-max text-center absolute text-3xl font-bold tracking-tight text-white sm:block sm:text-4xl lg:text-left">
